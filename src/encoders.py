c7b067f4d16ad5afacff5f9c4c00efcaac3f9258
adamjasinski <adam.jasinski@yahoo.ie>
2024-01-11 22:11:12 +0100

    The first version

---
+++ src/encoders.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+class OneHotEncoder:
+    @staticmethod
+    def encode(y, num_labels):
+        """
+        Convert class labels to one-hot encoded vectors.
+
+        This function takes an array of class labels and converts them into one-hot encoded
+        vectors. Each one-hot encoded vector represents the presence of a class label using a
+        1.0 in the corresponding position and 0.0 elsewhere.
+
+        Parameters:
+        - y (array-like): An array of class labels to be one-hot encoded.
+        - num_labels (int, optional): The total number of unique class labels. Defaults to 10.
+
+        Returns:
+        - one_hot (numpy.ndarray): A 2D numpy array where each column is a one-hot encoded
+        vector representing a class label.
+
+        Example usage:
+        >>> y = [0, 2, 1, 3, 0]
+        >>> one_hot_enc(y, num_labels=4)
+        array([[1., 0., 0., 0.],
+            [0., 0., 1., 0.],
+            [0., 1., 0., 0.],
+            [0., 0., 0., 1.],
+            [1., 0., 0., 0.]])
+        """
+        #one_hot = np.zeros((len(y), num_labels))
+        #for i, num in enumerate(y):
+        #    one_hot[i, num] = 1
+        one_hot = np.eye(num_labels)[y]
+        return one_hot
--- src/neural.py
+++ src/neural.py
@@ -1,7 +1,9 @@
+from typing import Callable
 import numpy as np
 
 class Neural:
     def __init__(self, input_size, hidden1_size, hidden2_size, output_size):
+        """Initialize a neural network"""
         self._data = None
         self.input_size = input_size
         self.hidden1_size = hidden1_size
@@ -42,13 +44,23 @@ class Neural:
         b3 = np.zeros((1, output_size)) # size 1 x output
         return W1, W2, W3, b1, b2, b3
 
-    def train(self, X_train, y_train, learning_rate = 0.1, epochs = 10, batch_size = 64):
-        """Train the neural network by running the backpropagation algorith"""
+    def train(self, X_train, y_train, learning_rate = 0.1, epochs = 10, batch_size = 64, progress_func=None):
+        """
+        Train the neural network by running the backpropagation algorith
+        
+        Parameters:
+        - X_train (numpy.ndarray): Input data (observations)
+        - y_train (numpy.ndarray): Target data
+        - learning_rate (float): Learning rate
+        - epochs (int): Number of iterations of the algorithm
+        - batch_size (int): Batch size in each step of the algorithm
+        - progress_func (Callable): callback function being called after iteration of the algorithm
+        """
         # Training loop
         for epoch in range(epochs):
             for i in range(0, len(X_train), batch_size):
                 x_batch = X_train[i:i + batch_size]
-                y_batch = y_train_onehot[i:i + batch_size]
+                y_batch = y_train[i:i + batch_size]
 
                 # Forward pass
                 z1 = np.dot(x_batch, self.W1) + self.b1
@@ -77,18 +89,19 @@ class Neural:
                 self.W3 -= learning_rate * dW3
                 self.b3 -= learning_rate * db3
 
-            # Calculate accuracy on the training set
-            z1 = np.dot(X_train, self.W1) + self.b1
-            a1 = Neural.sigmoid(z1)
-            z2 = np.dot(self.a1, self.W2) + self.b2
-            a2 = Neural.sigmoid(z2)
-            z3 = np.dot(self.a2, self.W3) + self.b3
-            a3 = Neural.softmax(z3)
+            # # Calculate accuracy on the training set
+            # z1 = np.dot(X_train, self.W1) + self.b1
+            # a1 = Neural.sigmoid(z1)
+            # z2 = np.dot(a1, self.W2) + self.b2
+            # a2 = Neural.sigmoid(z2)
+            # z3 = np.dot(a2, self.W3) + self.b3
+            # a3 = Neural.softmax(z3)
 
-            predictions = np.argmax(a3, axis=1)
-            train_accuracy = np.mean(predictions == y_train)
+            # predictions = np.argmax(a3, axis=1)
+            # train_accuracy = np.mean(predictions == y_train)
 
-        return train_accuracy
+            # if progress_func is not None and isinstance(progress_func, Callable):
+            #     progress_func(epoch, train_accuracy)
 
     def score(self, X, y):
         # Calculate accuracy on the training set
@@ -100,8 +113,8 @@ class Neural:
         a3 = Neural.softmax(z3)
 
         predictions = np.argmax(a3, axis=1)
-        train_accuracy = np.mean(predictions == y)
-        return train_accuracy
+        accuracy = np.mean(predictions == y)
+        return accuracy
 
     def predict(self, X):
         # Calculate accuracy on the test set
